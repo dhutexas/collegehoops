bd02d46c9ad69fd85b685db50aa39af4fd563c1d
Derek Hanson <dhanson@utexas.edu>
2021-03-01 04:43:30 +0000

    update column selection for when column does not exist in historical data

--- R/get_box_score_players.R
+++ R/get_box_score_players.R
@@ -10,6 +10,8 @@
 #' @importFrom jsonlite fromJSON
 #'
 
+espn_game_id = 401169632
+
 get_box_score_players <- function(espn_game_id) {
 
   game_json <- httr::GET(url = glue::glue("http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/summary?event={espn_game_id}")) %>%
@@ -29,13 +31,16 @@ get_box_score_players <- function(espn_game_id) {
     dplyr::filter(!.data$didNotPlay)
 
   player_box <- dplyr::bind_cols(players_df, stats_df) %>%
-    dplyr::select(athlete.id, athlete.guid, athlete.displayName, athlete.shortName,
-                  athlete.jersey, athlete.headshot.href, athlete.position.name,
-                  athlete.position.abbreviation, team.id, team.slug,
-                  team.location, team.name, team.abbreviation,
-                  team.color, team.alternateColor, team.logo,
-                  MIN:PTS)
+    dplyr::select(one_of(c('athlete.id', 'athlete.guid', 'athlete.displayName',
+                  'athlete.shortName',
+                  'athlete.jersey', 'athlete.headshot.href', 'athlete.position.name',
+                  'athlete.position.abbreviation', 'team.id', 'team.slug',
+                  'team.location', 'team.name', 'team.abbreviation',
+                  'team.color', 'team.alternateColor', 'team.logo',
+                  'MIN','FG','3PT','FT','OREB','DREB','REB','AST','STL','BLK',
+                  'TO','PF', 'PTS')))
 
   return(player_box)
 
 }
+
